1727b7437a678a9c19c928ab58545bd07e7e5c43
egogoboy <egor2005sudakov@gmail.com>
2025-04-08 11:52:27 +0300

    final

--- classes/Main.java
+++ classes/Main.java
@@ -2,6 +2,43 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
+    public static Figure getFigure(Scanner in) {
+        Figure result;
+        while (true) {
+
+            System.out.print("Enter type of figure:\n"
+                           + "1 - square\n"
+                           + "2 - circle\n"
+                           + "3 - triangle\n> ");
+            int choise = in.nextInt();
+            System.out.print("Enter coordinates of center: ");
+            double x = in.nextDouble();
+            double y = in.nextDouble();
+
+            switch (choise) {
+                case 1, 3: {
+                    System.out.print("Enter size of side: ");
+                    double side_size = in.nextDouble();
+                    if (choise == 1)
+                        result =(new Square(x, y, side_size));
+                    else 
+                        result = new Triangle(x, y, side_size);
+                    break;
+                }
+                case 2: {
+                    System.out.print("Enter radius: ");
+                    double radius = in.nextDouble();
+                    result = (new Square(x, y, radius));
+                    break;
+                }
+                default: {
+                    System.out.println("Unknow figure type");
+                }
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         try (Scanner in = new Scanner(System.in)) {
             System.out.print("Enter number of figures: ");
@@ -11,14 +48,15 @@ public class Main {
             for (int i = 0; i < n; ++i) {
                 System.out.print("Enter type of figure:\n"
                                + "1 - square\n"
-                               + "2 - circle\n> ");
+                               + "2 - circle\n"
+                               + "3 - triangle\n> ");
                 int choise = in.nextInt();
                 System.out.print("Enter coordinates of center: ");
                 double x = in.nextDouble();
                 double y = in.nextDouble();
 
                 switch (choise) {
-                    case 1: {
+                    case 1, 3: {
                         System.out.print("Enter size of side: ");
                         double side_size = in.nextDouble();
                         figures.add(new Square(x, y, side_size));
@@ -43,7 +81,7 @@ public class Main {
                                + "1 - print all squares\n"
                                + "2 - print all data\n"
                                + "3 - print all distances to origin\n"
-                               + "4 - print distance between all figures\n"
+                               + "4 - print distance between two figures\n"
                                + "5 - exit\n> ");
                 int choise = in.nextInt();
 
@@ -59,17 +97,26 @@ public class Main {
                                 System.out.println("Figure type: square");
                             } else if (figure instanceof Circle) {
                                 System.out.println("Figure type: circle");
-                            } else
+                            } else if (figure instanceof Triangle) {
+                                System.out.println("Figure type: triangle");
+                            }
+
+                            System.out.println("Coordinates: " + figure.getX() + " " + figure.getY());
                         }
                     }
                     case 3: {
-
+                        for (Figure figure : figures) {
+                            System.out.println(figure.distanceToOrigin());
+                        }
                     }
                     case 4: {
-
+                        Figure first = getFigure(in);
+                        Figure second = getFigure(in);
+                        
+                        System.out.println("Distance: " + Figure.getDistanceFromTo(first, second));
                     }
                     case 5: {
-
+                        exit = true;
                     }
                 }
             }
