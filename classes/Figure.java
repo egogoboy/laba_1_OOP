807d3bcac72fe6edaf017b1fc67e854d0753b8c4
egogoboy <egor2005sudakov@gmail.com>
2025-04-08 10:54:38 +0300

    Added Figure constructor

--- classes/Figure.java
+++ classes/Figure.java
@@ -2,6 +2,11 @@ abstract class Figure implements Distance {
     protected double x;
     protected double y;
 
+    public Figure(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
     public double getX() {
         return this.x;
     }
